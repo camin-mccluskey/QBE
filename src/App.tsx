a89936465d96ba34070d39b008f94b7a07c64c89
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2023-01-16 11:00:12 +0000

    Add notification registration and permissions on app l load

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { registerRootComponent } from 'expo';
+import { PermissionStatus } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
+import { Notification } from 'expo-notifications';
 import { StatusBar } from 'expo-status-bar';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -11,6 +14,7 @@ import Navigation from './navigation';
 import { QuestionContextProvider } from './store/QuestionContext';
 import DismissKeyboard from './components/DismissKeyboard';
 import useUserOnboarded from './hooks/useUserOnboarded';
+import { useEffect, useState } from 'react';
 
 if (
   Platform.OS === 'android' &&
@@ -23,6 +27,31 @@ const App: React.FC = () => {
   const isLoadingComplete = useCachedResources();
   const [isUserOnboarded] = useUserOnboarded();
   const colorScheme = useColorScheme();
+  const [notificationPermissions, setNotificationPermissions] = useState<PermissionStatus>(
+    PermissionStatus.UNDETERMINED,
+  );
+
+  const requestNotificationPermissions = async () => {
+    const { status } = await Notifications.requestPermissionsAsync();
+    setNotificationPermissions(status);
+    return status;
+  };
+
+  // todo - this is the place we do the deep linking
+  const handleNotification = (notification: Notification) => {
+    const { title } = notification.request.content;
+    console.warn(title);
+  };
+
+  useEffect(() => {
+    requestNotificationPermissions();
+  }, []);
+
+  useEffect(() => {
+    if (notificationPermissions !== PermissionStatus.GRANTED) return;
+    const listener = Notifications.addNotificationReceivedListener(handleNotification);
+    return () => listener.remove();
+  }, [notificationPermissions]);
 
   if (!isLoadingComplete) {
     return null;
